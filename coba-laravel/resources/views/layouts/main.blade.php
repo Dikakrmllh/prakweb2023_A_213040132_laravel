8149d64b7246157a0f1ec4b515e8b36e212147e4
Dikakrmllh <karomalloh23@gmail.com>
2023-11-17 20:39:08 +0700

    17.Dashboard UI

--- coba-laravel/resources/views/layouts/main.blade.php
+++ coba-laravel/resources/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Biodata Blog | {{ $title }}</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -14,7 +15,7 @@
     <!-- My Style -->
     <link rel="stylesheet" href="/css/style.css">
 
-    <title>Biodata Blog | {{ $title }}</title>
+    
   </head>
   <body>
 
